4ce70da1645468672228133f0bf67e6f548da55d
Demut <uddarendeli1@gmail.com>
2023-02-23 09:21:28 +0300

    Cleaning the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,13 @@
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import backgroundMap from "../assets/images/backgroundMap.png";
 import './Home.css'
 
 export const Home = () => {
-	const [pageSize, setPageSize] = useState(0);
-	const imageRef = useRef(null);
 
-	useLayoutEffect(() => {
-		document.title = "Poke Archive | Home";
-
-		setTimeout(() => {
-			const { height } = imageRef.current.getBoundingClientRect();
-			setPageSize(height - 4 * 15);
-		}, 300);
-
-	}, [imageRef.current]);
-
-	useEffect(() => {
-		const resizeForImage = () => {
-			const { height } = imageRef.current.getBoundingClientRect();
-			setPageSize(height - 4 * 15);
-		};
-
-		window.addEventListener('resize', resizeForImage);
-	
-		return () => {
-			window.removeEventListener('resize', resizeForImage);
-		}
-	}, [])
+	const size = `calc(100vh - 80px - 10rem)`;
 
 	return (
-		<div style={{ minHeight: `${pageSize}px` }} className="page home">
-			<img
-				className="home__background"
-				ref={imageRef}
-				src={backgroundMap}
-				alt="background, map of Konto area"
-			/>
+		<div style={{ minHeight: size }} className="page home">
 			<div
-				style={{ height: `${pageSize}px` }}
+				style={{ height: size }}
 				className="home__main"
 			>
 				<h1 className="home__title">Home</h1>
